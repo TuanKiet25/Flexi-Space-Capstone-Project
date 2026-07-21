9a839a8b1e6a134390f53629d71e899f748d6f50
TuanKiet25 <kietntse183810@fpt.edu.vn>
2026-07-21 12:01:21 +0700

    [update]: Latitude & Longitude for space

--- FlexiSpace.Application/ViewModels/Responses/Space/GetSpaceByIdRP.cs
+++ FlexiSpace.Application/ViewModels/Responses/Space/GetSpaceByIdRP.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace FlexiSpace.Application.ViewModels.Responses.Space
 {
-    public class GetSpaceByIdRP : CreateSpaceRQ
+    public class GetSpaceByIdRP : CreateSpaceRP
     {
         public new long Id { get; set; }
         public List<PictureURLVModel> PictureURLs { get; set; } = new List<PictureURLVModel>();
